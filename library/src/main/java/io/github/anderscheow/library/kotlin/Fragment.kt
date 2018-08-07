e90ac36d049ebf582a1ebc5d45a593faa8593140
Anders Cheow <anders.cheow@shoppertise.com>
2018-08-07 11:46:48 +0800

    Null check on context or activity on Fragments Parse context or activity to a class after null check

--- library/src/main/java/io/github/anderscheow/library/kotlin/Fragment.kt
+++ library/src/main/java/io/github/anderscheow/library/kotlin/Fragment.kt
@@ -1,5 +1,25 @@
 package io.github.anderscheow.library.kotlin
 
+import android.app.Activity
+import android.content.Context
 import android.support.v4.app.Fragment
 
 fun Fragment.isNotThere(): Boolean = (activity?.isFinishing ?: true) || isDetached
+
+fun Fragment.withContext(block: (Context) -> Unit) {
+    context?.let(block)
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Fragment.withContextAs(block: (T) -> Unit) {
+    (context as? T)?.let(block)
+}
+
+fun Fragment.withActivity(block: (Activity) -> Unit) {
+    activity?.let(block)
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Fragment.withActivityAs(block: (T) -> Unit) {
+    (activity as? T)?.let(block)
+}

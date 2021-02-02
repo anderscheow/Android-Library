e3632e871573ab27f9136299761ce09c0cff5a03
Anders Cheow <heekeong.cheow@togalimited.com>
2021-02-02 19:19:16 +0800

    Fixed unresolved reference

--- sample/src/main/java/io/github/anderscheow/android_library/MainSampleActivity.kt
+++ sample/src/main/java/io/github/anderscheow/android_library/MainSampleActivity.kt
@@ -2,14 +2,11 @@ package io.github.anderscheow.android_library
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main_sample.*
 
 class MainSampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_sample)
-
-        textView.start()
     }
 }
